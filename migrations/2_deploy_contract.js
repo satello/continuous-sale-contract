8fd413bb088cfc80f51df6cdee031a995c30c9e6
Ferit Tunçer <ferit@cryptolab.net>
2019-01-01 21:22:19 +0300

    refactor(migrations): add constructor args

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,10 +2,9 @@
 var ContinuousIICO = artifacts.require("./ContinuousIICO.sol");
 
 // JavaScript export
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
   // Deployer is the Truffle wrapper for deploying
   // contracts to the network
-
   // Deploy the contract to the network
-  deployer.deploy(ContinuousIICO);
+  deployer.deploy(ContinuousIICO, web3.utils.numberToHex(accounts[1]), 5, 5);
 };
