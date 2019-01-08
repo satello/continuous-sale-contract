b64e3e5c50acf97d917f8e7f7e930c6b53db1c4e
Ferit Tunçer <ferit@cryptolab.net>
2019-01-08 17:51:55 +0300

    style(truffle): linter fixes

--- truffle.js
+++ truffle.js
@@ -1,38 +1,38 @@
-var Web3 = require("web3");
-var HDWalletProvider = require("truffle-hdwallet-provider");
+var HDWalletProvider = require('truffle-hdwallet-provider')
+
 var mnemonic =
-  "emerge cabbage panel need lens sweet assault benefit broken lunch insect differ";
+  'emerge cabbage panel need lens sweet assault benefit broken lunch insect differ'
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   compilers: {
     solc: {
-      version: "^0.4.25"
+      version: '^0.4.25'
     }
   },
   mocha: {
-    reporter: "eth-gas-reporter",
+    reporter: 'eth-gas-reporter',
     reporterOptions: {
-      currency: "USD"
+      currency: 'USD'
     }
   },
 
   networks: {
+    development: {
+      host: 'localhost',
+      network_id: '*',
+      port: 8545
+    },
     kovan: {
-      provider: new HDWalletProvider(
-        mnemonic,
-        "https://kovan.infura.io/v3/344bdb3c652c4ce6acc12f10a7557ba6"
-      ),
-      network_id: 42,
+      confirmations: 2,
       gas: 4200000,
       gasPrice: 20000000000,
-      confirmations: 2
-    },
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*"
+      network_id: 42,
+      provider: new HDWalletProvider(
+        mnemonic,
+        'https://kovan.infura.io/v3/344bdb3c652c4ce6acc12f10a7557ba6'
+      )
     }
   }
-};
+}
