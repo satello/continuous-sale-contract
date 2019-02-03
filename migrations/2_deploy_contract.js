82cf3c98df01e9b1de7cc18df74fdcc7adc679ae
Ferit Tunçer <ferit@cryptolab.net>
2019-02-03 21:20:29 +0300

    chore(migrations): disable

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,16 +1,16 @@
 // Fetch the contract data
-var ContinuousSale = artifacts.require('./ContinuousSale.sol')
+// var ContinuousSale = artifacts.require('./ContinuousSale.sol')
 
 // JavaScript export
 module.exports = function(deployer, network, accounts) {
   // Deployer is the Truffle wrapper for deploying
   // contracts to the network
   // Deploy the contract to the network
-  deployer.deploy(
-    ContinuousSale,
-    accounts[1],
-    5,
-    5,
-    Math.floor(Date.now() / 1000)
-  )
+  // deployer.deploy(
+  //   ContinuousSale,
+  //   accounts[1],
+  //   5,
+  //   5,
+  //   Math.floor(Date.now() / 1000)
+  // )
 }
